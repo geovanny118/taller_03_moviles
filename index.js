abf801e10810dd67626cf39bd7db4baa07e5a2ae
Geovanni Rojas <grojas@loatech.co>
2023-03-08 16:30:03 -0500

    se termina el taller 3

--- index.js
+++ index.js
@@ -14,3 +14,37 @@ function structureIfElse(request) {
     document.getElementById("answerStructureIfElse").style.display = "none";
   }
 }
+
+function structureSwitchCase(request) {
+  switch(request){
+    case 'red':
+      document.getElementById("answerStructureSwitch").style.color = 'red';
+      break;
+    case 'green':
+      document.getElementById("answerStructureSwitch").style.color = 'green';
+      break;
+    case 'blue':
+      document.getElementById("answerStructureSwitch").style.color = 'blue';
+      break;
+    default:
+      document.getElementById("answerStructureSwitch").style.color = 'black';
+  }
+}
+
+function structureFor(number) {
+  let answer = 1;
+  for(i=1; i<=number; i++){
+    answer *= i;
+  }
+  document.getElementById("answerFor").innerHTML = `El factorial de ${number} es ${answer}`;
+}
+
+function structureWhile(limit) {
+  let number = 1;
+  let text = '';
+  while(number<limit){
+    text += "<br>The number is " + number;
+    number++;
+  }
+  document.getElementById("answerWhile").innerHTML = text; 
+}
